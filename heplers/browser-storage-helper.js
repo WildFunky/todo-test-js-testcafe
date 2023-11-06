512e04355ab921cefda6292c4b9d6a1840287804
Aleksandr Subach <a.subach@a1qa.com>
2023-11-06 15:44:32 +0400

    Removed unused parameters

--- heplers/browser-storage-helper.js
+++ heplers/browser-storage-helper.js
@@ -2,11 +2,11 @@ import { ClientFunction } from 'testcafe';
 
 const getLocalStorage = ClientFunction(() => window.localStorage.getItem('angular2-todos'))
 
-export async function getNumberOfTodosInLocalStorage(t) {
+export async function getNumberOfTodosInLocalStorage() {
     return JSON.parse(await getLocalStorage())
         .length;
 }
-export async function getNumberOfCompletedTodosInLocalStorage(t) {
+export async function getNumberOfCompletedTodosInLocalStorage() {
     return JSON.parse(await getLocalStorage())
         .filter(todo => todo['completed'] === true)
         .length;
